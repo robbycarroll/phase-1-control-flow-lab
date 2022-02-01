58e538e0c9b3bf26d345390c3170fd2e52a2cb5e
Robert Carroll <robertcarrollsales@gmail.com>
2022-02-01 10:07:44 -0500

    commit tree 4f5540fd20b215dbfa291295c1ddab18b6be8888

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let result
+  if (distance <= 400) {
+    result = 'This one is on me!';
+    return result;
+  }
+  else if (distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500)
+    return 'No can do.';
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? (city = "Ok, sounds good.") : (city = "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous': tip = 'Thank you so much.';
+      break;
+    case 'not as generous': tip = 'Thank you.';
+      break;
+    default: tip = 'Bye.';
+      break;
+  }
+  return tip;
 }
